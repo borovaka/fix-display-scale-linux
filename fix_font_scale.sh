710b559e113ef8cd80a7508492ae8d9f31494755
Borislav Anchev <borislav.anchev@sportal.bg>
2019-10-26 23:11:02 +0300

    Scale cursor

--- fix_font_scale.sh
+++ fix_font_scale.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCALE_FACTOR=1.4
+CURSOR_SIZE=32
 
 sleep 3
 export DISPLAY=:1
@@ -8,7 +9,9 @@ RESOLUTION=$(xrandr --listmonitors | cut -d' ' -f4 | cut -d'/' -f1 | sort -nr | 
 
 if [[ "$RESOLUTION" -gt "1920" ]]; then 
 	bash -c "export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus && gsettings set org.gnome.desktop.interface text-scaling-factor $SCALE_FACTOR"
+	bash -c "export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus && gsettings set org.gnome.desktop.interface cursor-size $CURSOR_SIZE"
 else
 	bash -c "export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus && gsettings set org.gnome.desktop.interface text-scaling-factor 1"
+	bash -c "export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus && gsettings set org.gnome.desktop.interface cursor-size 24"
 fi
 
